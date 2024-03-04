ad5a8c298bad9a74fef4328540109dcfa71a541a
Stephane van Gulick <stephane.vangulick@swisscom.com>
2024-03-04 10:52:02 +0100

    added code samples and slides

---
+++ fr/StephaneVanGulick/Introduction aux Classes/Demo_02_Propriétées.ps1
@@ -0,0 +1,21 @@
+﻿class maison {
+
+    [String]$Couleur
+    [int]$Habitants
+
+}
+
+new-object maison
+
+$maMaison = [maison]::New()
+
+$OSDParameterHash = @{"TestVar1"="plop";"TestVar2"="woop"}
+[String]$Commande = "-File OSDScript.ps1"
+foreach($para in $OSDParameterHash.Keys){
+    $Commande += " -$($para) " + $OSDParameterHash[$para]
+}
+
+$Commande
+
+$ArgumentList = '-NoLogo -NoExit',"-File OSDScript.ps1 {0}" -f $Global:OSDParametersHash
+Start-Process -WorkingDirectory "C:\Users\JM2K69\AppData\Local\Temp" -FilePath PowerShell.exe -ArgumentList $ArgumentList
---
+++ fr/StephaneVanGulick/Introduction aux Classes/Demo_03_Methodes.ps1
@@ -0,0 +1,29 @@
+﻿class maison {
+
+    #Propriétés
+    [string]$Couleur
+    [int]$Habitants
+
+    #Constructeurs
+
+    maison() {
+        #Les constructeurs n'ont pas de d'indicateur de retour
+        $this.Couleur = "Rouge"
+    }
+
+    #Methodes
+
+    [int] GetNombreHabitants() {
+        return $this.Habitants
+    }
+
+    [String] GetCouleur() {
+        Return $this.Couleur
+    }
+
+    #Methode qui ne retourne pas de valeur (VOID)
+    [void] SetCouleur([String]$Couleur) {
+        $this.Couleur = $Couleur
+    }
+
+}
